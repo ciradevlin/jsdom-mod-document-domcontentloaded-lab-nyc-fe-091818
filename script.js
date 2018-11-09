122ce93fa4a83ba97009db4b474f9d4ebdfab9d8
Cira Devlin <cira.devlin@leveluprpo.com>
2018-11-09 19:24:39 +0000

    automatically backed up by learn

---
+++ script.js
@@ -0,0 +1,3 @@
+<p id="text">Eventing in JavaScript makes things look sharp.</p>
+    <script type="text/javascript">
+      document.querySelector("body").addEventListener("click", e => console.log("Reggae, Reggae!"));</script>
